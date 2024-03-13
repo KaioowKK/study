ba9d117b440a196adf52953331d9fd13dd8055d9
Kaioow <kaiandrokoia@gmail.com>
2024-03-13 04:11:59 -0400

    JS Style Guide

--- js/javinha.js
+++ js/javinha.js
@@ -2,3 +2,9 @@ let text = '{ "employees" : [' +
 '{ "firstName":"John" , "lastName":"Doe" },' +
 '{ "firstName":"Anna" , "lastName":"Smith" },' +
 '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+if (time < 20) {
+    greeting = "Good day";
+  } else {
+    greeting = "Good evening";
+  }
