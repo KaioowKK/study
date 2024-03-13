c7167649b7335b7772dd7ef65177ec6f4f4aaa72
Kaioow <kaiandrokoia@gmail.com>
2024-03-13 15:38:53 -0400

    JS Object Constructors

--- js/javinha.js
+++ js/javinha.js
@@ -82,3 +82,12 @@ obj.add = 5;
 obj.subtract = 1;
 obj.increment;
 obj.decrement;
+
+new String()    // A new String object
+new Number()    // A new Number object
+new Boolean()   // A new Boolean object
+new Object()    // A new Object object
+new Array()     // A new Array object
+new RegExp()    // A new RegExp object
+new Function()  // A new Function object
+new Date()      // A new Date object
