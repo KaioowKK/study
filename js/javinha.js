6ac80bb2e2c22bc9e15cef7ae0b9fb2a04ad28bb
Kaioow <kaiandrokoia@gmail.com>
2024-03-13 04:27:37 -0400

    JS Performance

--- js/javinha.js
+++ js/javinha.js
@@ -12,3 +12,13 @@ if (time < 20) {
   let x = 0.1;
   let y = 0.2;
   let z = x + y            // the result in z will not be 0.3
+
+  const person = [];
+person["firstName"] = "John";
+person["lastName"] = "Doe";
+person["age"] = 46;
+person.length;      // person.length will return 0
+person[0];          // person[0] will return undefined
+
+let l = arr.length;
+for (let i = 0; i < l; i++)
