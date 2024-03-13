8da3975ceb7241caf17175dbc102f8d26e52e5f1
Kaioow <kaiandrokoia@gmail.com>
2024-03-13 16:31:43 -0400

    JS Object Iterables

--- js/javinha.js
+++ js/javinha.js
@@ -102,3 +102,19 @@ function Person(first, last, age, eyecolor) {
   Person.prototype.name = function() {
     return this.firstName + " " + this.lastName;
   };
+
+  // Create an Object
+myNumbers = {};
+
+// Make it Iterable
+myNumbers[Symbol.iterator] = function() {
+  let n = 0;
+  done = false;
+  return {
+    next() {
+      n += 10;
+      if (n == 100) {done = true}
+      return {value:n, done:done};
+    }
+  };
+}
