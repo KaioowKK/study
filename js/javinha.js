90d5e5b673da85019dffa4c367ebfac3ae25993d
Kaioow <kaiandrokoia@gmail.com>
2024-03-16 15:28:19 -0400

    JS Function Parameters

--- js/javinha.js
+++ js/javinha.js
@@ -167,3 +167,11 @@ obj.add = 5;
 obj.subtract = 1;
 obj.increment;
 obj.decrement;
+
+function sum(...args) {
+  let sum = 0;
+  for (let arg of args) sum += arg;
+  return sum;
+}
+
+let xw = sum(4, 9, 16, 25, 29, 100, 66, 77);
