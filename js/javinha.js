9914b49ec72458b9b3d274421339f88028a3ceb4
Kaioow <kaiandrokoia@gmail.com>
2024-03-16 15:48:55 -0400

    JS Function Bind

--- js/javinha.js
+++ js/javinha.js
@@ -201,3 +201,18 @@ const person1 = {
 
 // This will return "Mary Doe":
 person.fullName.apply(person1);
+
+const person = {
+  firstName:"John",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+const member = {
+  firstName:"Hege",
+  lastName: "Nilsen",
+}
+
+let fullName = person.fullName.bind(member);
