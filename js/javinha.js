c260c178b8a18e4b91332fb566d63a8afd6caa99
Kaioow <kaiandrokoia@gmail.com>
2024-03-16 15:54:19 -0400

    JS Function Closures

--- js/javinha.js
+++ js/javinha.js
@@ -216,3 +216,14 @@ const member = {
 }
 
 let fullName = person.fullName.bind(member);
+
+const add = (function () {
+  let counter = 0;
+  return function () {counter += 1; return counter}
+})();
+
+add();
+add();
+add();
+
+// the counter is now 3
