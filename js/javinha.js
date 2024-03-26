3add0833c9d09f3f8835e74f6f1bdfd34b5bcfd2
Kaioow <kaiandrokoia@gmail.com>
2024-03-26 16:42:13 -0400

    JS Cookies

--- js/javinha.js
+++ js/javinha.js
@@ -376,3 +376,10 @@ if (confirm("Press a button!")) {
 function myFunction() {
   alert('Hello');
 }
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
