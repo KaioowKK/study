eda4256d1ef2c0b009bfb6e5a8a967ffedc18037
Kaioow <kaiandrokoia@gmail.com>
2024-03-28 14:39:54 -0400

    JS AJAX Database

--- js/javinha.js
+++ js/javinha.js
@@ -531,3 +531,16 @@ function showHint(str) {
   xmlhttp.send();
   }
 }
+
+function showCustomer(str) {
+  if (str == "") {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  }
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("txtHint").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "getcustomer.php?q="+str);
+  xhttp.send();
+}
