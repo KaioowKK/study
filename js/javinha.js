6cd2fb63da2b0087107b87aabc4d0957fb268cf0
Kaioow <kaiandrokoia@gmail.com>
2024-03-28 16:20:15 -0400

    JS JSON HTML

--- js/javinha.js
+++ js/javinha.js
@@ -615,3 +615,19 @@ xmlhttp.onload = function() {
 }
 xmlhttp.open("GET", "demo_file_array.php", true);
 xmlhttp.send();
+
+const dbParam = JSON.stringify({table:"customers",limit:20});
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  const myObj = JSON.parse(this.responseText);
+  let text = "<select>"
+  for (let x in myObj) {
+    text += "<option>" + myObj[x].name + "</option>";
+  }
+  text += "</select>"
+  document.getElementById("demo").innerHTML = text;
+  }
+  
+xmlhttp.open("POST", "json_demo_html_table.php", true);
+xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xmlhttp.send("x=" + dbParam);
