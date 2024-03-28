d69597033144868f28f6d48021e148a2a8065142
Kaioow <kaiandrokoia@gmail.com>
2024-03-28 14:22:55 -0400

    JS AJAX PHP

--- js/javinha.js
+++ js/javinha.js
@@ -503,3 +503,17 @@ function myFunction(xml) {
   }
   document.getElementById("demo").innerHTML = table;
 }
+
+function showHint(str) {
+  if (str.length == 0) {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  } else {
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function() {
+      document.getElementById("txtHint").innerHTML = this.responseText;
+    }
+  xmlhttp.open("GET", "gethint.php?q=" + str);
+  xmlhttp.send();
+  }
+}
