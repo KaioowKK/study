04b42d89e172233ac649c436b18d531fb0f1029d
Kaioow <kaiandrokoia@gmail.com>
2024-03-28 13:26:52 -0400

    JS AJAX Response

--- js/javinha.js
+++ js/javinha.js
@@ -468,3 +468,15 @@ function myFunction1(xhttp) {
 function myFunction2(xhttp) {
   // action goes here
 }
+
+const xmlDoc = xhttp.responseXML;
+const xxw = xmlDoc.getElementsByTagName("ARTIST");
+
+let txt = "";
+for (let i = 0; i < x.length; i++) {
+  txt += xxw[i].childNodes[0].nodeValue + "<br>";
+}
+document.getElementById("demo").innerHTML = txt;
+
+xhttp.open("GET", "cd_catalog.xml");
+xhttp.send();
