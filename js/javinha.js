c7103d0c6508e9a9b1e2b8eab1337d57b486ae21
Kaioow <kaiandrokoia@gmail.com>
2024-03-28 14:32:30 -0400

    JS AJAX ASP

--- js/javinha.js
+++ js/javinha.js
@@ -517,3 +517,17 @@ function showHint(str) {
   xmlhttp.send();
   }
 }
+
+function showHint(str) {
+  if (str.length == 0) {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  } else {
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function() {
+      document.getElementById("txtHint").innerHTML = this.responseText;
+    }
+  xmlhttp.open("GET", "gethint.asp?q=" + str);
+  xmlhttp.send();
+  }
+}
