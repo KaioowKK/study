c6d2245bb20ceaa6687816e247f1433278cb84d5
Kaioow <kaiandrokoia@gmail.com>
2024-03-28 16:03:19 -0400

    JS JSON PHP

--- js/javinha.js
+++ js/javinha.js
@@ -607,3 +607,11 @@ xmlhttp.onload = function() {
 };
 xmlhttp.open("GET", "json_demo.txt");
 xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  const myObj = JSON.parse(this.responseText);
+  document.getElementById("demo").innerHTML = myObj[2];
+}
+xmlhttp.open("GET", "demo_file_array.php", true);
+xmlhttp.send();
