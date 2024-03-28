8460bba80c6e7b516290cab6c9604193b81f20c3
Kaioow <kaiandrokoia@gmail.com>
2024-03-28 15:03:23 -0400

    JS JSON vs XML

--- js/javinha.js
+++ js/javinha.js
@@ -556,3 +556,23 @@ function displayCD(i) {
 }
 
 person = {name:"John", age:31, city:"New York"};
+
+/* {"employees":[
+  { "firstName":"John", "lastName":"Doe" },
+  { "firstName":"Anna", "lastName":"Smith" },
+  { "firstName":"Peter", "lastName":"Jones" }
+]}
+
+<employees>
+  <employee>
+    <firstName>John</firstName> <lastName>Doe</lastName>
+  </employee>
+  <employee>
+    <firstName>Anna</firstName> <lastName>Smith</lastName>
+  </employee>
+  <employee>
+    <firstName>Peter</firstName> <lastName>Jones</lastName>
+  </employee>
+</employees>
+
+*/
