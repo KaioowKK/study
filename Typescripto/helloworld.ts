33c9d6bfb5af4cdbf5f5065913929a6b5bf417a3
Kaioow <kaiandrokoia@gmail.com>
2024-04-05 11:50:19 -0400

    TS Arrays

--- Typescripto/helloworld.ts
+++ Typescripto/helloworld.ts
@@ -17,3 +17,16 @@ if(typeof w === 'object' && w !== null) {
   (w as { runANonExistentMethod: Function }).runANonExistentMethod();
 }
 // Although we have to cast multiple times we can do a check in the if to secure our type and have a safer casting
+
+const numbers = [1, 2, 3]; // inferred to type number[]
+
+numbers.push(4); // no error
+
+// comment line below out to see the successful assignment 
+//numbers.push("2"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+
+console.log(numbers);
+
+let head: number = numbers[0]; // no error
+
+console.log(head);
