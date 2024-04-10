1933a41dd716dc91a81416bcb6aaf8cb6536bd51
Kaioow <kaiandrokoia@gmail.com>
2024-04-10 10:54:40 -0400

    TS Basic Generics

--- Typescripto/helloworld.ts
+++ Typescripto/helloworld.ts
@@ -109,3 +109,25 @@ class Person {
 
 const person = new Person("Jane");
 console.log(person.getName());
+
+class NamedValue<T> {
+  private _value: T | undefined;
+
+  constructor(private name: string) {}
+
+  public setValue(value: T) {
+    this._value = value;
+  }
+
+  public getValue(): T | undefined {
+    return this._value;
+  }
+
+  public toString(): string {
+    return `${this.name}: ${this._value}`;
+  }
+}
+
+let value = new NamedValue<number>('myNumber');
+value.setValue(10);
+console.log(value.toString()); // myNumber: 10
